d8c8253a54f2e90b6bc66e19030a48aba286107c
PapiLuee <41023873+LueDev@users.noreply.github.com>
2023-10-23 01:57:41 -0400

    all tests passing

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  let seenNumbers = new Set();
+
+  for(let num of array){
+      if(seenNumbers.has(target - num)){
+          return true;
+      }
+      seenNumbers.add(num);
+  }
+
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+
+  We're iterating through the array once so the time complexity is O(n).
+  The set operations 'has' and 'add' have an avg time complexity of O(1).
+  There time complexity is O(n * 1) = O(n)
+
+  We're using seenNumbers to store all n numbers in the worst-case scenario. 
+  Therefore, worst case is n and Space Complexity is O(n)
+
 */
 
 /* 
   Add your pseudocode here
+  Create a set we will push nums in the array to. 
+  If seenNumbers contains target - num, then return true.
+  Otherwise, add the current num in the array to seenNumbers. 
+  Return false if seenNumbers never contains target - num result in array
 */
 
 /*
